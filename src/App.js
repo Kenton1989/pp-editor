74b504fc8b389e378415b150ff17cbd76d14c838
Kenton <38125586+Kenton1989@users.noreply.github.com>
2022-04-10 01:03:10 +0800

    add menu bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import { inputSaveFile, outputSaveFile } from "./save-file-io";
 import { Layout, Menu, PageHeader } from "antd";
+import MenuBar from "./components/menubar";
 import "./App.css";
 
 const { SubMenu } = Menu;
+
 const { Header, Footer, Sider, Content } = Layout;
+
 function App() {
   // const [text, setText] = useState("");
   return (
@@ -15,13 +18,7 @@ function App() {
             <img src="/logo192.png" alt="logo" />
             <span>Parabox Editor</span>
           </div>
-          <Menu theme="dark" mode="horizontal" selectable={false}>
-            <SubMenu key="file" title="File">
-              <Menu.Item key="new">New</Menu.Item>
-              <Menu.Item key="open">Open</Menu.Item>
-              <Menu.Item key="save">Save</Menu.Item>
-            </SubMenu>
-          </Menu>
+          <MenuBar onItemTriggered={(key) => console.log(key)} />
         </Header>
         <Content className="stretch-height"></Content>
       </Layout>
---
+++ src/components/menubar.js
@@ -0,0 +1,64 @@
+import { Menu } from "antd";
+import Hotkeys from "react-hot-keys";
+
+const { SubMenu } = Menu;
+
+export default function MenuBar(props) {
+  const { onItemTriggered = nothing, ...otherProps } = props;
+
+  const menuProps = Object.assign(
+    {
+      theme: "dark",
+      mode: "horizontal",
+      selectable: false,
+      onClick: ({ key }) => onItemTriggered(key),
+      forceSubMenuRender: true, // to load shortcut
+    },
+    otherProps
+  );
+
+  function ShortCut(props) {
+    const { id, shortCut } = props;
+    return (
+      <Hotkeys keyName={shortCut} onKeyDown={() => onItemTriggered(id)}>
+        <span className="action-item-shortcut">{shortCut}</span>
+      </Hotkeys>
+    );
+  }
+
+  return (
+    <Menu {...menuProps}>
+      <SubMenu key="file" title="File">
+        <Menu.Item key={ID.new}>New</Menu.Item>
+        <Menu.Item key={ID.open}>Open</Menu.Item>
+        <Menu.Item key={ID.save}>
+          Save <ShortCut id={ID.save} shortCut={SHORTCUT.save} />
+        </Menu.Item>
+      </SubMenu>
+      <SubMenu key="edit" title="Edit">
+        <Menu.Item key={ID.undo}>
+          Undo <ShortCut id={ID.undo} shortCut={SHORTCUT.undo} />
+        </Menu.Item>
+        <Menu.Item key={ID.redo}>
+          Redo <ShortCut id={ID.redo} shortCut={SHORTCUT.redo} />
+        </Menu.Item>
+      </SubMenu>
+    </Menu>
+  );
+}
+
+const ID = {
+  new: "new",
+  open: "open",
+  save: "save",
+  undo: "undo",
+  redo: "redo",
+};
+
+const SHORTCUT = {
+  save: "ctrl+shift+s",
+  undo: "ctrl+z",
+  redo: "ctrl+shift+z",
+};
+
+function nothing() {}
